10cc78aa9c6f3d4878c584061bd45616187444c8
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2019-06-27 12:09:12 -0600

    Realizo el js que prueba si es una palabra palíndroma

---
+++ palindromo.js
@@ -0,0 +1,34 @@
+const btnPalindromo = document.getElementById('palíndromo')
+const response = document.getElementById('response');
+
+function texto() {
+  let word = document.getElementById('text').value.toLowerCase();
+  word = word.replace(/ /g, "");
+
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] != word[word.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+
+}
+function confirmation() {
+  if (texto()) {
+    let p = document.createElement('p');
+    p.innerHTML += 'Esto es palíndromo';
+    response.innerHTML = '';
+    response.appendChild(p);
+
+  } else {
+    let p = document.createElement('p');
+    p.innerHTML += 'Esto no es palíndromo';
+    response.innerHTML = '';
+    response.appendChild(p);
+
+  }
+}
+
+
+btnPalindromo.addEventListener('click', texto);
+btnPalindromo.addEventListener('click', confirmation);
